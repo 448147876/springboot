71d34422510ce74dfb360bf4971e1ddd6565fdf9
448147876@qq.com <tongzhijie12345>
2019-03-14 19:34:07 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
